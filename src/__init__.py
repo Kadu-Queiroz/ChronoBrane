aac687efbf05d600a552b6bb4c2bf8c50cc44cc6
Carlos Eduardo Queiroz <108004435+Kadu-Queiroz@users.noreply.github.com>
2025-10-06 17:55:35 -0300

    Delete src directory

--- src/__init__.py
+++
@@ -1,7 +0,0 @@
-# ChronosBrane Core Package
-
-__version__ = "0.1.0"
-__description__ = "Core predictive engine for ethical-collapse-aware futures simulation (ChronosBrane)"
-
-from .core import *
-from .simulation import *
